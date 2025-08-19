71574909dec616ebb60844247a4c782e37ed08a9
yujhenchen <yujhen.chen93@gmail.com>
2025-08-19 10:35:04 +0200

    feat: add theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { ReactNode } from "react";
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en" data-theme="dracula">
+      <body>{children}</body>
+    </html>
+  );
 }
