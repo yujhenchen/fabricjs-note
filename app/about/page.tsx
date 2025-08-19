5f4a071dc3953411c87c57c2eeab6da9e7eef924
yujhenchen <yujhen.chen93@gmail.com>
2025-08-19 10:59:09 +0200

    feat: add about page draft

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "../components/container";
+import { PageLayout } from "../components/pageLayout";
+
+export default function About() {
+  return (
+    <Container>
+      <PageLayout>
+        <h1 className="text-3xl font-bold">About me</h1>
+      </PageLayout>
+    </Container>
+  );
+}
