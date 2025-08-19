356c7216f05c7550b0aaedbf82c2c79573441f48
yujhenchen <yujhen.chen93@gmail.com>
2025-08-19 10:59:24 +0200

    feat: add home page draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function HomePage() {
-  return <h1 className="text-3xl font-bold text-blue-500">Hello, Next.js!</h1>;
+import { Container } from "./components/container";
+import { Footer } from "./components/footer";
+import { PageLayout } from "./components/pageLayout";
+import { Plus } from "./components/plus";
+
+export default function Home() {
+  return (
+    <Container>
+      <PageLayout>
+        <h1 className="text-3xl font-bold">Have anything in mind?</h1>
+      </PageLayout>
+    </Container>
+  );
 }
