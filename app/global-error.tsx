631187ae1b7f40a942bed98ee25ba1af73d98333
yujhenchen <yujhen.chen93@gmail.com>
2025-08-19 09:26:29 +0200

    feat: implement basic fallback pages

---
+++ app/global-error.tsx
@@ -0,0 +1,21 @@
+"use client"; // Error boundaries must be Client Components
+
+export default function GlobalError({
+  // error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    // global-error must include html and body tags
+    <html lang="en">
+      <body>
+        <h2>Something went wrong!</h2>
+        <button type="button" onClick={() => reset()}>
+          Try again
+        </button>
+      </body>
+    </html>
+  );
+}
