b5fa8c0fad2fec8b47c32fab12ed32dd88ca1410
yujhenchen <yujhen.chen93@gmail.com>
2025-08-21 15:37:56 +0200

    feat: use svg component

--- app/components/list.tsx
+++ app/components/list.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./button";
+import { SVG } from "./svg";
 
 export type ListType = "tools" | "shapes";
 
@@ -12,50 +13,23 @@ const ShapesButtons = () => {
   return (
     <>
       <Button tooltip="circle">
-        <svg
-          role="img"
-          aria-label="circle"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
+        <SVG ariaLabel="circle">
           <circle cx="12" cy="12" r="9" stroke="currentColor" />
-        </svg>
+        </SVG>
       </Button>
 
       <Button tooltip="triangle">
-        <svg
-          role="img"
-          aria-label="triangle"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
+        <SVG ariaLabel="triangle">
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M2.697 16.126c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126Z"
           />
-        </svg>
+        </SVG>
       </Button>
 
       <Button tooltip="rect">
-        <svg
-          role="img"
-          aria-label="rect"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
+        <SVG ariaLabel="rect">
           <rect
             x="3.75"
             y="3.75"
@@ -66,22 +40,13 @@ const ShapesButtons = () => {
             stroke="currentColor"
             fill="none"
           />
-        </svg>
+        </SVG>
       </Button>
 
       <Button tooltip="text">
-        <svg
-          role="img"
-          aria-label="text"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
+        <SVG ariaLabel="text">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h12M12 6v12" />
-        </svg>
+        </SVG>
       </Button>
     </>
   );
@@ -91,41 +56,23 @@ const ToolsButtons = () => {
   return (
     <>
       <Button tooltip="import JSON">
-        <svg
-          role="img"
-          aria-label="import JSON"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
+        <SVG ariaLabel="import JSON">
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
           />
-        </svg>
+        </SVG>
       </Button>
 
       <Button tooltip="export SVG">
-        <svg
-          role="img"
-          aria-label="export SVG"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
+        <SVG ariaLabel="export SVG">
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m6.75 12-3-3m0 0-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
           />
-        </svg>
+        </SVG>
       </Button>
     </>
   );
