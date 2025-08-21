9ee5f366e81b2ab9a8fac6a87d20fff4f649a316
yujhenchen <yujhen.chen93@gmail.com>
2025-08-21 15:33:55 +0200

    feat: simplify home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import { FabricJSCanvas } from "./components/fabricJSCanvas";
 import { ContentContainer } from "./containers/contentContainer";
 import { PageContainer } from "./containers/pageContainer";
+import { Editor } from "./editors/Editor";
 
 export default function Home() {
   return (
     <PageContainer>
       <ContentContainer>
-        <h1 className="text-3xl font-bold absolute top-4 left-4">Have anything in mind?</h1>
-        <FabricJSCanvas className="mt-24 sm:mt-16" />
+        <Editor />
       </ContentContainer>
     </PageContainer>
   );
