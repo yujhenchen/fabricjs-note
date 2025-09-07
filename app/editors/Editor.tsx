f16c18b22bf5c9a52caa1374e5cf11770bacbd96
yujhenchen <yujhen.chen93@gmail.com>
2025-09-07 13:52:30 +0800

    feat: access child component refs from parent, add test button

--- app/editors/Editor.tsx
+++ app/editors/Editor.tsx
@@ -2,10 +2,16 @@
 
 import { useRef } from "react";
 import { Canvas } from "../canvas/Canvas";
+import type { Handler } from "../canvas/handlers/handler";
+import type { FabricCanvas } from "../canvas/models/object.model";
+import { Button } from "../components/button";
 import { List } from "../components/list";
 
 export const Editor = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // const canvasRef = useRef<CanvasInstance>(null);
+  const handlerRef = useRef<Handler | null>(null);
+  const fabricCanvasRef = useRef<FabricCanvas | null>(null);
+
   const options = {
     backgroundColor: "#fdf6e3",
   };
@@ -13,8 +19,28 @@ export const Editor = () => {
   return (
     <div className="w-full h-full">
       <List listType="tools" title="Tools" className="flex-row place-content-end" />
-      <Canvas ref={canvasRef} canvasOptions={options} className="mt-4" />
+      <Canvas
+        handlerRef={handlerRef}
+        fabricCanvasRef={fabricCanvasRef}
+        canvasOptions={options}
+        className="mt-4"
+      />
       <List listType="shapes" title="Shapes" className="flex-row" />
+
+      {/* TODO: remove this test used button */}
+      <Button
+        onClick={() => {
+          handlerRef.current?.add("circle", true, {
+            radius: 50,
+            fill: "blue",
+            left: 100,
+            top: 100,
+            centeredScaling: true,
+          });
+        }}
+      >
+        Circle
+      </Button>
     </div>
   );
 };
